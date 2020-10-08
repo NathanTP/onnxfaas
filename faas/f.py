d0aa2fa096210affcbe796f56df5f6c671ba504e
Nathan Pemberton <nathanp@berkeley.edu>
2020-10-08 14:52:01 -0700

    Added intermediate serialization option (pickle+redis)

--- faas/f.py
+++ faas/f.py
@@ -4,32 +4,81 @@ import models.ferplus as ferplus
 import models.bertsquad as bertsquad 
 import time
 import argparse
+import redis
+import pickle
+
+class prof:
+    def __init__(self):
+        self.total = 0.0
+        self.ncall = 0
+
+    def increment(self, n):
+        self.total += n
+        self.ncall += 1
+
+    def total(self):
+        return self.total
+
+    def mean(self):
+        return self.total / self.ncall
+
+# ms
+timeScale = 1000
 
 @contextmanager
 def timer(name, timers):
-   start = time.time()
+   start = time.time() * timeScale 
    try:
        yield
    finally:
        if name not in timers:
-           timers[name] = 0.0
-       timers[name] += time.time() - start
+           timers[name] = prof()
+       timers[name].increment((time.time()*timeScale) - start)
 
 
-def runTest(state, niter=1):
+def runTest(state, niter=1, profTimes=None, serialize=False):
     inputs = state.inputs()
 
+    kv = redis.Redis(password="Cd+OBWBEAXV0o2fg5yDrMjD9JUkW7J6MATWuGlRtkQXk/CBvf2HYEjKDYw4FC+eWPeVR8cQKWr7IztZy")
+
     for repeat in range(niter):
-        for testIn in inputs:
-            processed = state.pre(testIn)
+        for i, testIn in enumerate(inputs):
+            iterID = "{}.{}".format(repeat, i)
+            if profTimes is not None:
+                with timer("pre", profTimes):
+                    processed = state.pre(testIn)
+                    if serialize:
+                        kv.set(iterID+".pre", pickle.dumps(processed))
 
-            # predict using the deployed model
-            modelOut = state.run(processed)
+                with timer("run", profTimes):
+                    if serialize:
+                        desProcessed = pickle.loads(kv.get(iterID+".pre"))
+                    else:
+                        desProcessed = processed
 
-            finalOut = state.post(modelOut)
+                    modelOut = state.run(desProcessed)
+
+                    if serialize:
+                        kv.set(iterID+".run", pickle.dumps(modelOut))
+
+                with timer("post", profTimes):
+                    if serialize:
+                        desModelOut = pickle.loads(kv.get(iterID+".run"))
+                    else:
+                        desModelOut = modelOut
+
+                    finalOut = state.post(modelOut)
+
+                    kv.set(iterID+".final", pickle.dumps(finalOut))
+
+                kv.delete(iterID+".pre", iterID+".run", iterID+".final")
+            else:
+                processed = state.pre(testIn)
+                modelOut = state.run(processed)
+                finalOut = state.post(modelOut)
 
 
-def main(model, profile=False, niter=1, use_gpu=True):
+def main(model, profile=False, niter=1, use_gpu=True, serialize=False):
     if profile:
         times = {}
         
@@ -43,19 +92,19 @@ def main(model, profile=False, niter=1, use_gpu=True):
                 state = model(profile=profile, provider="CPUExecutionProvider") 
 
         with timer("run", times):
-            runTest(state, niter=niter)
+            runTest(state, niter=niter, profTimes=times, serialize=serialize)
 
         prof_file = state.session.end_profiling()
         print("onnxruntime profile at: ", prof_file)
-        print("Times: ")
-        print(times)
+        print("Times (ms): ")
+        print({ name : v.mean() for name, v in times.items() })
     else:
         model.imports()
         if use_gpu:
             state = model(profile=profile, provider="CUDAExecutionProvider") 
         else:
             state = model(profile=profile, provider="CPUExecutionProvider") 
-        runTest(state, niter=niter)
+        runTest(state, niter=niter, serialize=serialize)
 
 
 if __name__ == "__main__":
@@ -65,6 +114,7 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--profile", action='store_true', help="Enable self-profiling")
     parser.add_argument("-n", "--niter", type=int, default=1, help="Number of test iterations to perform")
     parser.add_argument("-m", "--model", type=str, default="ferplus", help="Which model to run, either 'bertsquad' or 'ferplus'")
+    parser.add_argument("-s", "--serialize", action='store_true', help="Serialize data (and store in kv store) in between steps. You must have a redis server running for this.")
 
     args = parser.parse_args()
 
@@ -73,7 +123,7 @@ if __name__ == "__main__":
     elif args.model == "bertsquad":
         model = bertsquad.Model
 
-    main(model, profile=args.profile, niter=args.niter, use_gpu = not args.cpu)
+    main(model, profile=args.profile, niter=args.niter, use_gpu = not args.cpu, serialize=args.serialize)
 
     # Cprofile
     # profFile = "fer10kGPU.prof"
